a7984a8aaf7a52801f1b027f25c8e012c81187dc
varenn <studresvaren@gmail.com>
2018-04-04 12:15:23 +0300

    task

---
+++ src/com/example/proj/proje.java
@@ -0,0 +1,20 @@
+package com.example.proj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class proje {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n= Integer.parseInt(reader.readLine());
+        for (int i = 1; i <n ; i++) {
+            if (n%i==0) System.out.println(i);
+
+        }
+
+    }
+
+}
